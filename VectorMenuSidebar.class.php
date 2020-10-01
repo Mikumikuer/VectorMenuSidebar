4f4ce530539897ffbfd4c4eb49cd63c4738c6dc5
夕舞八弦 <1677759063@qq.com>
2020-10-02 00:39:42 +0800

    fix js

--- VectorMenuSidebar.class.php
+++ VectorMenuSidebar.class.php
@@ -15,19 +15,12 @@ class VectorMenuSidebar {
         }
         
         if ($wgVectorMenuSidebar===true) {
-                echo '<script>window.toolbox=document.querySelector("#p-tb ul").innerHTML;var a=document.querySelectorAll("#mw-panel > *");for (var i=0;i<a.length;i++){if(a[i].id!="p-logo"){a[i].parentNode.removeChild(a[i])}}</script>';
-                $MenuSidebar = wfMessage( 'MenuSidebar' )->parse();
-                echo '<div id="MenuSidebar">'.$MenuSidebar;
-                #echo '<div style="display:none">'.$wgOut->getSkin()->getSkinName().'</div>';
-                echo '<p id="vmsTB">'.wfMessage( 'toolbox' )->plain().'</p><ul id="MSToolbox"></ul>';
-                echo '<script>document.querySelector("#MSToolbox").innerHTML=window.toolbox;childlist = document.querySelectorAll("#MenuSidebar li>ul");for(var i=0;i<childlist.length;i++){childlist[i].parentElement.classList.add("child")};</script>';
-                echo '<script>childlist = document.querySelectorAll("#MenuSidebar > ul#MSToolbox > li");for(var i=0;i<childlist.length;i++){if(childlist[i].innerHTML === ""){childlist[i].parentNode.removeChild(childlist[i])}};</script>';
-		
-		if ($wgShowAfterMenuSidebar===true) {
-                        $MenuSidebarTail = wfMessage('MenuSidebarAfter')->parse();
-                        echo '<div>'.$MenuSidebarTail.'</div>';
+                echo '<div id="MenuSidebar" style="display:none">'. wfMessage('MenuSidebar')->parse() . '<p id="vmsTB">' . wfMessage('toolbox')->plain() . '</p><ul id="MSToolbox"></ul>';
+                if ($wgShowAfterMenuSidebar === true) {
+                        echo '<div>' . wfMessage('MenuSidebarAfter')->parse() . '</div>';
                 }
                 echo '</div>';
+                echo '<script>window.addEventListener("DOMContentLoaded",(function(){document.querySelector("#MSToolbox").innerHTML=document.querySelector("#p-tb ul").innerHTML,document.querySelectorAll("#mw-panel > *:not(#p-logo)").forEach((function(e){return e.remove()}));for(var e=document.querySelectorAll("#MenuSidebar li>ul"),n=0;n<e.length;n++)e[n].parentElement.classList.add("child");e=document.querySelectorAll("#MenuSidebar > ul#MSToolbox > li");for(var o=0;o<e.length;o++)""===e[o].innerHTML&&e[o].parentNode.removeChild(e[o]);var r=document.querySelector("#MenuSidebar");r.setAttribute("style",""),document.querySelector("#mw-panel").appendChild(r)}));</script>';
         }
         return true;
     }
