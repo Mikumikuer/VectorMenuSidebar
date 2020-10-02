347464f663a1791d16ee73ff92dd1ee417c391fa
StarHeartHunt <starheart233@gmail.com>
2020-10-02 17:10:50 +0800

    Fix edit toolbar overflow

--- VectorMenuSidebar.class.php
+++ VectorMenuSidebar.class.php
@@ -15,12 +15,12 @@ class VectorMenuSidebar {
         }
         
         if ($wgVectorMenuSidebar===true) {
-                echo '<div id="MenuSidebar" style="display:none">'. wfMessage('MenuSidebar')->parse() . '<p id="vmsTB">' . wfMessage('toolbox')->plain() . '</p><ul id="MSToolbox"></ul>';
+                $out->addHTML('<div id="MenuSidebar" style="display:none">'. wfMessage('MenuSidebar')->parse() . '<p id="vmsTB">' . wfMessage('toolbox')->plain() . '</p><ul id="MSToolbox"></ul>');
                 if ($wgShowAfterMenuSidebar === true) {
-                        echo '<div>' . wfMessage('MenuSidebarAfter')->parse() . '</div>';
+                        $out->addHTML('<div>' . wfMessage('MenuSidebarAfter')->parse() . '</div>');
                 }
-                echo '</div>';
-                echo '<script>window.addEventListener("DOMContentLoaded",(function(){document.querySelector("#MSToolbox").innerHTML=document.querySelector("#p-tb ul").innerHTML,document.querySelectorAll("#mw-panel > *:not(#p-logo)").forEach((function(e){return e.remove()}));for(var e=document.querySelectorAll("#MenuSidebar li>ul"),n=0;n<e.length;n++)e[n].parentElement.classList.add("child");e=document.querySelectorAll("#MenuSidebar > ul#MSToolbox > li");for(var o=0;o<e.length;o++)""===e[o].innerHTML&&e[o].parentNode.removeChild(e[o]);var r=document.querySelector("#MenuSidebar");r.setAttribute("style",""),document.querySelector("#mw-panel").appendChild(r)}));</script>';
+                $out->addHTML('</div>');
+                $out->addHTML('<script>window.addEventListener("DOMContentLoaded",(function(){document.querySelector("#MSToolbox").innerHTML=document.querySelector("#p-tb ul").innerHTML,document.querySelectorAll("#mw-panel > *:not(#p-logo)").forEach((function(e){return e.remove()}));for(var e=document.querySelectorAll("#MenuSidebar li>ul"),n=0;n<e.length;n++)e[n].parentElement.classList.add("child");e=document.querySelectorAll("#MenuSidebar > ul#MSToolbox > li");for(var o=0;o<e.length;o++)""===e[o].innerHTML&&e[o].parentNode.removeChild(e[o]);var r=document.querySelector("#MenuSidebar");r.setAttribute("style",""),document.querySelector("#mw-panel").appendChild(r)}));</script>');
         }
         return true;
     }
